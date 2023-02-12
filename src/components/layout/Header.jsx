37f5c703bcb98d46d82b116d7749cab481f3d408
Jean Rondon <jeanrdev@gmail.com>
2023-02-11 21:24:10 -0400

    navbar toggle terminado (falta estructurado de carpetas/archivos y clean code)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/header.sass";
 import vitalineaLogo from "../../assets/vitalinea-logo.png";
 import facebookLogo from "../../assets/facebook-logo.svg";
@@ -7,6 +7,11 @@ import youtubeLogo from "../../assets/youtube-logo.svg";
 import menuIcon from "../../assets/menu-icon.svg";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (isOpen) console.log("abierto");
+  else console.log("close");
+
   return (
     <header>
       <div className="container">
@@ -53,10 +58,50 @@ export const Header = () => {
             </ul>
           </div>
 
-          <div className="menuIcon">
+          <div className="menuIcon" onClick={() => setIsOpen(!isOpen)}>
             <img src={menuIcon} alt="Menu Icon" />
           </div>
         </nav>
+      </div>
+
+      <div className={isOpen ? "menuOpen" : "menuClosed"}>
+        <div className="navbarContainerLinkToggle">
+          <ul className="navLinks">
+            <li>
+              <a href="#">Nuevos productos</a>
+            </li>
+
+            <li>
+              <a href="#">Disfruta cuidarte</a>
+            </li>
+
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbarContainerRedToggle">
+          <ul className="redesSociales">
+            <li>
+              <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                <img src={facebookLogo} alt="Facebook Vitalínea Logo" />
+              </a>
+            </li>
+
+            <li>
+              <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                <img src={instagramLogo} alt="Instagram Vitalínea Logo" />
+              </a>
+            </li>
+
+            <li>
+              <a href="https://youtube.com" target="_blank" rel="noreferrer">
+                <img src={youtubeLogo} alt="Youtube Vitalínea Logo" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
